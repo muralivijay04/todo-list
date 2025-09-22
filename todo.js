adb29852bca336925e6daad27725b0defbc8adef
muralivijay04 <bmuralivijayreddy@gmail.com>
2025-09-22 22:40:16 +0530

    first commit

---
+++ todo.js
@@ -0,0 +1,35 @@
+const inputBox=document.getElementById("inputbox");
+const listContainer=document.getElementById("list-container");
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span =document.createElement("span");
+        span.innerHTML="DELETE";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    savedata();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        savedata();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savedata();
+    }
+},false);
+function savedata(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showtask(){
+    listContainer.innerHTMl=localStorage.getItem("data");
+}
+showtask();
